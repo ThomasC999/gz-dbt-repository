34ff82d98fd35cd00e563a7f672681e9840cd7b6
thomas.collin999@gmail.com <thomas.collin999@gmail.com>
2024-10-29 15:22:52 +0000

    ajout colonnes

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -9,7 +9,6 @@ WITH fusion AS(
         b.shipping_fee,
         b.logcost,
         b.ship_cost
-
     FROM {{ ref('int_sales_margin') }} AS a
     INNER JOIN {{ ref('stg_raw__ship') }} AS b
     USING(orders_id))
@@ -17,10 +16,14 @@ WITH fusion AS(
 SELECT 
     date_date,
     orders_id,
-    revenue,
-    quantity,
-    purchase_cost,
-    margin,
-    ROUND(margin + shipping_fee - logcost - ship_cost, 2) AS operationnal_margin
+    SUM(revenue) AS revenue,
+    SUM(quantity) AS quantity,
+    SUM(purchase_cost) AS purchase_cost,
+    SUM(margin) AS margin,
+    ROUND(SUM(margin + shipping_fee - logcost - ship_cost), 2) AS operationnal_margin,
+    SUM(shipping_fee) AS shipping_fee,
+    SUM(logcost) AS logcost,
+    SUM(ship_cost) AS ship_cost
 FROM fusion
+GROUP BY date_date, orders_id
 ORDER BY date_date DESC, orders_id DESC
